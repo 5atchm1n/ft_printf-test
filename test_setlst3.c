73e5ebf5838f7d187bc6ac90c1ff5deccdfc06a6
Satcheen SHAKYA <sshakya@student.42.fr>
2021-01-12 18:15:40 +0100

    :construction: ft_printf test added

--- test_setlst3.c
+++ test_setlst3.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 13:38:44 by sshakya           #+#    #+#             */
-/*   Updated: 2021/01/12 15:09:51 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/01/12 16:43:12 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,22 @@ int		main(void)
 	num = 12345678;
 	fstr = "%010i this is a test %010i another test\n";
 	printf("-------------------------------------------\n");
-	printf("%s",fstr);
+	printf("format = %s",fstr);
 	printf("-------------------------------------------\n");
 	ft_printf(fstr, num, num);
 	printf("-------------------------------------------\n");
 	fstr = "%d this is a test %d another test\n";
-	printf("%s",fstr);
+	printf("format = %s",fstr);
 	printf("-------------------------------------------\n");
 	ft_printf(fstr, num, num);
 	printf("-------------------------------------------\n");
 	fstr = "%#x this is a test %#x another test\n";
-	printf("%s",fstr);
+	printf("format = %s",fstr);
 	printf("-------------------------------------------\n");
 	ft_printf(fstr, num, num);
-	fstr = "%10x this is a test %10x another test\n";
+	fstr = "%-10x this is a test %-10x another test\n";
 	printf("-------------------------------------------\n");
-	printf("%s",fstr);
+	printf("format = %s",fstr);
 	printf("-------------------------------------------\n");
 	ft_printf(fstr, num, num);
 	printf("-------------------------------------------\n");
