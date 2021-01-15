681ada0b62e21a805d8ed89ad012ce0c971b9f0a
sshakya <sshakya@student.42.fr>
2021-01-15 14:57:08 +0100

    :construction: added var size check to pftest
    *
    
    *

--- test_printf.c
+++ test_printf.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 08:54:28 by sshakya           #+#    #+#             */
-/*   Updated: 2021/01/14 17:40:05 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/01/15 14:52:49 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,7 +129,6 @@ str = "%-.5d";
 	printf("||\n");
 	i++;
 	printf("----------------------------------------------\n");
-	i++;
 	printf("\tTESTING PRECISION AND WIDTH\n");
 // TESTING PRECISION
 	i = 1;
@@ -299,6 +298,109 @@ str = "%-15.15d";
 	printf("||\n");
 
 	printf("----------------------------------------------\n");
+	printf("\tTESTING PRECISION AND WIDTH\n");
+// TESTING PRECISION
+	i = 1;
+	printf("----------------------------------------------\n");
+	printf("   || %-*s", wd, f);
+	printf("||%s\n", o);
+	printf("----------------------------------------------\n");
+	int in = 123456 ;
+	short int sin = 12;
+	long lin = 2147483647;
+	long long llin = 9223372036854775807;
+// 1
+	str = "%i";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, in);
+	printf("||");
+	printf("%i\n", in);
+	printf("----------------------------------------------\n");
+// 2
+	i++;
+	str = "%hi";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, sin);
+	printf("||");
+	printf("%hi\n", sin);
+	printf("----------------------------------------------\n");
+// 3
+	i++;
+	str = "%li";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, lin);
+	printf("||");
+	printf("%li\n", lin);
+	printf("----------------------------------------------\n");
+// 4
+	i++;
+	str = "%lli";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, llin);
+	printf("||");
+	printf("%lli\n", llin);
+	printf("----------------------------------------------\n");
+// 5
+	i++;
+	str = "%i";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, llin);
+	printf("||");
+	printf("%lli\n", llin);
+	printf("----------------------------------------------\n");
+// 6
+	i++;
+	str = "%hi";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, in);
+	printf("||");
+	printf("%i\n", in);
+	printf("----------------------------------------------\n");
+// 7
+	i++;
+	str = "%li";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, llin);
+	printf("||");
+	printf("%lli\n", llin);
+	printf("----------------------------------------------\n");
+// 8
+	i++;
+	str = "%lli";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, sin);
+	printf("||");
+	printf("%hi\n", sin);
+	printf("----------------------------------------------\n");
+// 9
+	i++;
+	str = "%llu";
+	printf(" %d ||%10s", i, str);
+	printf("||");
+	printf(str, n);
+	printf("||\n");
+	printf("%lli\n", llin);
+	printf("----------------------------------------------\n");
+
+	printf("----------------------------------------------\n");
+
+	printf("size of char = %lu\n", sizeof(char));
+	printf("size of unsigned char = %lu\n", sizeof(unsigned char));
+	printf("size of short  = %lu\n", sizeof(short));
+	printf("size of unsigned short = %lu\n", sizeof(unsigned short));
+	printf("size of int = %lu\n", sizeof(int));
+	printf("size of long = %ju\n", sizeof(long));
+	printf("size of long long = %ju\n", sizeof(long long));
+	printf("size of ulong = %ju\n", sizeof(unsigned long));
+	printf("size of ulong long = %ju\n", sizeof(unsigned long long));
 	printf("size of intmax_t = %ju\n", sizeof(intmax_t));
 	printf("size of intptr_t = %ju\n", sizeof(intptr_t));
 	printf("size of uintptr_t = %ju\n", sizeof(uintptr_t));
@@ -307,12 +409,6 @@ str = "%-15.15d";
 	printf("size of int16_t = %ju\n", sizeof(int16_t));
 	printf("size of int32_t = %ju\n", sizeof(int32_t));
 	printf("size of int64_t = %ju\n", sizeof(int64_t));
-	printf("size of int = %lu\n", sizeof(int));
-	printf("size of long = %ju\n", sizeof(long));
-	printf("size of long long = %ju\n", sizeof(long long));
-	printf("size of ulong = %ju\n", sizeof(unsigned long));
-	printf("size of ulong long = %ju\n", sizeof(unsigned long long));
-
 /*
 	printf("----------------------------------------------\n");
 	printf("\tTESTING N COUNTER\n");
