62b67457d9c449b7935a5f06810ae4f1fe2e8465
sshakya <sshakya@student.42.fr>
2021-01-16 02:36:27 +0100

    :construction:split test files
    *
    
    *

--- test_final.c
+++ test_final.c
@@ -6,7 +6,7 @@
 /*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/19 08:54:28 by sshakya           #+#    #+#             */
-/*   Updated: 2021/01/15 00:18:54 by sshakya          ###   ########.fr       */
+/*   Updated: 2021/01/16 01:49:20 by sshakya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int			main(void)
 {
 	char	*str;
-	char	*test;
+//	char	*test;
 	char	*o;
 	char	*f;
 	int		wd;
@@ -34,63 +34,91 @@ int			main(void)
 	ft_printf("   ||%-*s", wd, f);
 	ft_printf("||%s\n", o);
 
-	// 1
+// 1
 	ft_printf("----------------------------------------------\n");
 	i++;
 	str = "%-10d";
 	n = 10;
-	ft_printf(" %d ||%10s", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
-	// 2
+// 2
 	ft_printf("----------------------------------------------\n");
 	i++;
-	str = "%012d";
+	str = "%+08d";
 	n = -123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
 // 3
 	ft_printf("----------------------------------------------\n");
 	i++;
-	str = "%+012d";
-	n = 123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	str = "%+09d";
+	n = -123456789;
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
 // 4
 	ft_printf("----------------------------------------------\n");
 	i++;
-	str = "%-#19x";
-	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
+	str = "%+010d";
+	n = -123456789;
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
 // 5
 	ft_printf("----------------------------------------------\n");
 	i++;
-	str = "%#x";
-	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
+	str = "%+011d";
+	n = -123456789;
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
-	ft_printf("----------------------------------------------\n");
 
-	ft_printf("\tTESTING PRECISION\n");
+// 6
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%+09d";
+	n = 123456789;
+	ft_printf(" %d || %10i||%10s", i, n, str);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 7
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%+010d";
+	n = 123456789;
+	ft_printf(" %d || %10i||%10s", i, n, str);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 8
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%+011d";
+	n = 123456789;
+	ft_printf(" %d || %10i||%10s", i, n, str);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+
 // TESTING PRECISION
+	ft_printf("----------------------------------------------\n");
+	ft_printf("\tTESTING PRECISION\n");
 	i = 1;
 	ft_printf("----------------------------------------------\n");
 	ft_printf("   || %-*s", wd, f);
 	ft_printf("||%s\n", o);
 	ft_printf("----------------------------------------------\n");
-str = "%-.5d";
+	str = "%-.5d";
 	n = 123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
@@ -99,50 +127,22 @@ str = "%-.5d";
 	i++;
 	str = "%0.5d";
 	n = 123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
-// 3
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%#.19x";
-	n = 1123456789;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, n);
-	ft_printf("||\n");
-// 4
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%-#.5x";
-	n = 1123456789;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, n);
-	ft_printf("||\n");
-// 5
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%#x";
-	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, n);
-	ft_printf("||\n");
-	i++;
-	ft_printf("----------------------------------------------\n");
-	i++;
-	ft_printf("\tTESTING PRECISION AND WIDTH\n");
 // TESTING PRECISION
-	i = 1;
 	ft_printf("----------------------------------------------\n");
+	ft_printf("\tTESTING PRECISION AND WIDTH\n");
+	ft_printf("----------------------------------------------\n");
+	i = 1;
 	ft_printf("   || %-*s", wd, f);
 	ft_printf("||%s\n", o);
+// 1
 	ft_printf("----------------------------------------------\n");
-str = "%-15.15d";
+	str = "%-15.15d";
 	n = 123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
@@ -151,7 +151,7 @@ str = "%-15.15d";
 	i++;
 	str = "%15.20d";
 	n = 123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
@@ -160,39 +160,65 @@ str = "%-15.15d";
 	i++;
 	str = "%-#15.10x";
 	n = 1123456789;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
-// 3
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%#15.10x";
-	n = 1123456789;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, n);
-	ft_printf("||\n");
-// 4
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%-#20.15x";
-	n = 1123456789;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, n);
-	ft_printf("||\n");
-// 5
+// 4	
 	ft_printf("----------------------------------------------\n");
 	i++;
 	str = "%-10.5i";
 	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
+	ft_printf(" %d || %10i||%10s", i, n, str);
 	ft_printf("||");
 	ft_printf(str, n);
 	ft_printf("||\n");
 	i++;
-
+// TESTING PRECISION
+	ft_printf("----------------------------------------------\n");
+	ft_printf("\tTESTING PRECISION AND WIDTH\n");
+	ft_printf("----------------------------------------------\n");
+	unsigned int u;
+	i = 1;
+	ft_printf("   || %-*s", wd, f);
+	ft_printf("||%s\n", o);
+// 1	
+	ft_printf("----------------------------------------------\n");
+	str = "%-15.15u";
+	u = 4294967295;
+	ft_printf(" %d || %10u||%10s", i, u, str);
+	ft_printf("||");
+	ft_printf(str, u);
+	ft_printf("||\n");
+// 2
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%15.20u";
+	u = 4294967295;
+	ft_printf(" %d || %10u||%10s", i, u, str);
+	ft_printf("||");
+	ft_printf(str, u);
+	ft_printf("||\n");
+// 3
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-#15.10x";
+	u = 4199999999;
+	ft_printf(" %d || %10u||%10s", i, u, str);
+	ft_printf("||");
+	ft_printf(str, u);
+	ft_printf("||\n");
+// 4	
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-10.5u";
+	u = 4199999999;
+	ft_printf(" %d || %10u||%10s", i, u, str);
+	ft_printf("||");
+	ft_printf(str, u);
+	ft_printf("||\n");
+	i++;
+/*
 	// TESTING STRING FLAGS
 	ft_printf("----------------------------------------------\n");
 	ft_printf("\tTESTING STRING FLAGS\n");
@@ -249,49 +275,7 @@ str = "%-15.15d";
 	ft_printf("||");
 	ft_printf(str, test);
 	ft_printf("||\n");
-
-	ft_printf("----------------------------------------------\n");
-	ft_printf("\tTESTING POINTER FORMAT\n");
-	ft_printf("----------------------------------------------\n");
-	ft_printf("   || %-*s", wd, f);
-	ft_printf("||%s\n", o);
-	ft_printf("----------------------------------------------\n");
-// 1
-	i = 1;
-	str = "%-10p";
-	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, &n);
-	ft_printf("||\n");
-// 2
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%10p";
-	n = 123456789;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, &n);
-	ft_printf("||\n");
-// 3
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%-19p";
-	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, &n);
-	ft_printf("||\n");
-// 4
-	ft_printf("----------------------------------------------\n");
-	i++;
-	str = "%19p";
-	n = 10;
-	ft_printf(" %d ||%10s ", i, str);
-	ft_printf("||");
-	ft_printf(str, &n);
-	ft_printf("||\n");
-	ft_printf("----------------------------------------------\n");
+*/
 /*
 	ft_printf("----------------------------------------------\n");
 	ft_printf("\tTESTING N COUNTER\n");
---
+++ test_final2.c
@@ -0,0 +1,242 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_final2.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshakya <sshakya@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/16 00:40:03 by sshakya           #+#    #+#             */
+/*   Updated: 2021/01/16 01:30:35 by sshakya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../ft_printf.h"
+
+int			main(void)
+{
+	char	*str;
+	char	*o;
+	char	*f;
+	int		wd;
+	int		n;
+	int		i;
+	
+	ft_printf("----------------------------------------------\n");
+	ft_printf("-------- TESTING PRINTF RETURN ---------------\n");
+	ft_printf("----------------------------------------------\n");
+	
+	o = "output";
+	f ="format";
+	wd = 10;
+	i = 0;
+	ft_printf("   ||%-*s", wd, f);
+	ft_printf("||%s\n", o);
+	ft_printf("----------------------------------------------\n");
+
+	ft_printf("\tTESTING HEX\n");
+	ft_printf("----------------------------------------------\n");
+// 1
+	i++;
+	str = "%-#x";
+	n = 12345;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 2
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#x";
+	n = 12345;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 3
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#10x";
+	n = 12345;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 4
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#010x";
+	n = 2147483647;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 5
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#15x";
+	n = 2147483647;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 6
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-#15x";
+	n = 2147483647;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+
+	// TESTING PRECISION
+	ft_printf("----------------------------------------------\n");
+	ft_printf("----------------------------------------------\n");
+	ft_printf("\tTESTING PRECISION\n");
+	ft_printf("----------------------------------------------\n");
+	ft_printf("   || %-*s", wd, f);
+	ft_printf("||%s\n", o);
+	ft_printf("----------------------------------------------\n");
+// 1
+	i = 1;
+	str = "%#.15x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 2
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-#.5x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 3
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#x";
+	n = 10;
+	ft_printf(" %d ||%10s||%-10i", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+
+	ft_printf("----------------------------------------------\n");
+	ft_printf("\tTESTING PRECISION AND WIDTH\n");
+	ft_printf("----------------------------------------------\n");
+	i = 1;
+	str = "%#15.10x";
+	n = 123456;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+// 1
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-#15.10x";
+	n = 123456;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-#15.8x";
+	n = 123456;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#15.8x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#14.8x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#14.9x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%#13.10x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+
+	i++;
+	str = "%-#.x";
+	n = 1123456789;
+	ft_printf(" %d ||%10s ||%u", i, str, n);
+	ft_printf("||");
+	ft_printf(str, n);
+	ft_printf("||\n");
+
+// TESTING POINTER	
+	ft_printf("----------------------------------------------\n");
+	ft_printf("\tTESTING POINTER FORMAT\n");
+	ft_printf("----------------------------------------------\n");
+	ft_printf("   || %-*s", wd, f);
+	ft_printf("||%s\n", o);
+	ft_printf("----------------------------------------------\n");
+// 1
+	i = 1;
+	str = "%-10p";
+	n = 10;
+	ft_printf(" %d ||%10s ", i, str);
+	ft_printf("||");
+	ft_printf(str, &n);
+	ft_printf("||\n");
+// 2
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%10p";
+	n = 123456789;
+	ft_printf(" %d ||%10s ", i, str);
+	ft_printf("||");
+	ft_printf(str, &n);
+	ft_printf("||\n");
+// 3
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%-19p";
+	n = 10;
+	ft_printf(" %d ||%10s ", i, str);
+	ft_printf("||");
+	ft_printf(str, &n);
+	ft_printf("||\n");
+// 4
+	ft_printf("----------------------------------------------\n");
+	i++;
+	str = "%19p";
+	n = 10;
+	ft_printf(" %d ||%10s ", i, str);
+	ft_printf("||");
+	ft_printf(str, &n);
+	ft_printf("||\n");
+	ft_printf("----------------------------------------------\n");
+
+	return (0);
+}
